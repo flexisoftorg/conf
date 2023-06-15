2e6a8252cb3fe9f1bb8b2fc290b4ab0c3d400ade
Simen A. W. Olsen <so@bjerk.io>
2023-06-15 19:19:18 +0200

    untracked files on simenandre/configure-with-sanity: 875befd wip

---
+++ resources/kubernetes/namespace.ts
@@ -0,0 +1,12 @@
+import * as k8s from '@pulumi/kubernetes';
+import { provider } from '../shared/kubernetes/provider';
+
+const name = 'portal-prod';
+
+export const namespace = new k8s.core.v1.Namespace(
+  name,
+  {
+    metadata: { name },
+  },
+  { provider },
+);
