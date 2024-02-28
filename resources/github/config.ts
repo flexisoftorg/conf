e82611bc7f49314ef76bb7daa10cde73473e94b8
Brage Sekse Aarset <brage.aarset@gmail.com>
2024-02-28 15:09:10 +0200

    fix: consume pulumi environment (#426)

--- resources/github/config.ts
+++ resources/github/config.ts
@@ -1,6 +1,6 @@
 import * as pulumi from '@pulumi/pulumi';
 
-const config = new pulumi.Config();
+const config = new pulumi.Config('github');
 
-export const token = config.requireSecret('token');
+export const token = config.requireSecret('flexisoftorg-token');
 export const owner = config.require('owner');
