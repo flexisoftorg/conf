1f49e3ba3c5d310aedcc766539c5a00016b91b4b
Brage Sekse Aarset <brage.aarset@gmail.com>
2024-08-01 11:37:13 +0300

    feat: populate domain field in customers without custom domain in sanity, specify specific domains (#513)
    * feat: populate domain field in customers without custom domain in sanity
    
    * feat: add domains for all services to env
    
    * fix: make domains clean
    
    * fix: handle clean domains
    
    * sort imports
    
    * revert irrelevant change

--- resources/kubernetes/customer-config.ts
+++ resources/kubernetes/customer-config.ts
@@ -1,5 +1,6 @@
 import * as kubernetes from '@pulumi/kubernetes';
 import { customers } from '../get-customers';
+import { rootDomain } from '../shared/config';
 import { provider as kubernetesProvider } from '../shared/kubernetes/provider';
 import { namespace } from './namespace';
 
@@ -13,7 +14,35 @@ export const customerConfigMap = new kubernetes.core.v1.ConfigMap(
       },
     },
     data: {
-      CUSTOMERS: customers.apply(customers => JSON.stringify(customers)),
+      CUSTOMERS: customers.apply(customers => {
+        const customersWithDomain = customers.map(customer => {
+          const cleanRootDomain = rootDomain.slice(0, -1);
+
+          const domain = customer.domain
+            ? customer.domain
+            : `${customer.ident.current}.${cleanRootDomain}`;
+
+          const hasCustomDomain = Boolean(customer.domain?.trim());
+
+          // TODO: Revise this logic once it is safe to do so (every customer is using non custom domains)
+          const debitorPortalDomain = hasCustomDomain
+            ? `debitor.${domain}`
+            : domain;
+          const creditorPortalDomain = hasCustomDomain
+            ? domain
+            : `kred.${domain}`;
+          const apiDomain = `api.${domain}`;
+
+          return {
+            ...customer,
+            domain,
+            debitorPortalDomain,
+            creditorPortalDomain,
+            apiDomain,
+          };
+        });
+        return JSON.stringify(customersWithDomain);
+      }),
     },
   },
   { provider: kubernetesProvider },
