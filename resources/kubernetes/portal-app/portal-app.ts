f0ac474c7e905d8c926f794eb43411faee5bf4a9
Xillians <sandra.toftemo@bjerk.io>
2025-01-17 15:16:18 +0100

    feat: add portal app secrets

--- resources/kubernetes/portal-app/portal-app.ts
+++ resources/kubernetes/portal-app/portal-app.ts
@@ -1,3 +1,4 @@
+import * as kubernetes from '@pulumi/kubernetes';
 import * as pulumi from '@pulumi/pulumi';
 import { interpolate } from '@pulumi/pulumi';
 import { DeploymentComponent } from '../../components/deployment';
@@ -8,6 +9,21 @@ import { customerConfigMap } from '../customer-config';
 import { namespace } from '../namespace';
 
 const config = new pulumi.Config('portal-app');
+const AgGridLicenseKey = config.requireSecret('ag-grid-license-key');
+
+export const portalAppEnvSecrets = new kubernetes.core.v1.Secret(
+  'portal-api-env-secrets',
+  {
+    metadata: {
+      name: 'portal-app-env-secrets',
+      namespace: namespace.metadata.name,
+    },
+    data: {
+      AG_GRID_LICENSE_KEY: AgGridLicenseKey,
+    },
+  },
+  { provider: kubernetesProvider },
+);
 
 const cleanPortalAppDomain = portalAppDomain.slice(0, -1);
 
@@ -19,7 +35,10 @@ export const portalApp = new DeploymentComponent(
     host: cleanPortalAppDomain,
     namespace: namespace.metadata.name,
     port: 8000,
-    envFrom: [{ configMapRef: { name: customerConfigMap.metadata.name } }],
+    envFrom: [
+      { configMapRef: { name: customerConfigMap.metadata.name } },
+      { secretRef: { name: portalAppEnvSecrets.metadata.name } }
+    ],
     resources: {
       requests: {
         cpu: '250m',
