92a009dc936e5cc524e9c35eff8605c27756dfee
Brage Sekse Aarset <brage@bjerk.io>
2025-06-03 13:09:49 +0300

    feat: add more env variables to api

--- resources/kubernetes/api/api.ts
+++ resources/kubernetes/api/api.ts
@@ -1,15 +1,33 @@
+import * as kubernetes from '@pulumi/kubernetes';
 import * as pulumi from '@pulumi/pulumi';
 import { interpolate } from '@pulumi/pulumi';
-import { DeploymentComponent } from '../../components/deployment';
-import { apiDomain } from '../../config';
-import { artifactRepoUrl } from '../../shared/google/artifact-registry';
-import { provider as kubernetesProvider } from '../../shared/kubernetes/provider';
-import { namespace } from '../namespace';
-import { registrationAppSanityCredentials } from '../registration-app/sanity-credentials';
+import { DeploymentComponent } from '../../components/deployment.js';
+import { apiDomain } from '../../config.js';
+import { artifactRepoUrl } from '../../shared/google/artifact-registry.js';
+import { provider as kubernetesProvider } from '../../shared/kubernetes/provider.js';
+import { namespace } from '../namespace.js';
+import { redis } from '../portal-api/redis.js';
+import { registrationAppSanityCredentials } from '../registration-app/sanity-credentials.js';
 
 const config = new pulumi.Config('api');
 
 const cleanApiDomain = apiDomain.slice(0, -1);
+
+const cookieSecret = config.requireSecret('cookie-secret');
+
+export const apiEnvSecrets = new kubernetes.core.v1.Secret(
+  'api-env-secrets',
+  {
+    metadata: {
+      name: 'api-env-secrets',
+      namespace: namespace.metadata.name,
+    },
+    data: {
+      COOKIE_SECRET: cookieSecret,
+    },
+  },
+  { provider: kubernetesProvider },
+);
 
 export const Api = new DeploymentComponent(
   'api',
@@ -20,6 +38,16 @@ export const Api = new DeploymentComponent(
     namespace: namespace.metadata.name,
     envFrom: [
       { secretRef: { name: registrationAppSanityCredentials.metadata.name } },
+    ],
+    env: [
+      {
+        name: 'REDIS_URL',
+        value: interpolate`${redis.service.metadata.name}.${redis.service.metadata.namespace}.svc.cluster.local:6379`,
+      },
+      {
+        name: 'SELF_URL',
+        value: `https://${cleanApiDomain}`,
+      },
     ],
     port: 8000,
     resources: {
