b5e4037af8d95d04513b2839489ef073e729dbc2
Brage Sekse Aarset <brage@bjerk.io>
2025-06-03 16:59:58 +0300

    fix: ignore id changes

--- resources/shared/google/legacy-dns.ts
+++ resources/shared/google/legacy-dns.ts
@@ -1,7 +1,7 @@
-import * as gcp from '@pulumi/gcp';
-import {apiServices} from '../../google/api-services.js';
-import {provider} from '../../google/provider.js';
-import {ipAddress} from './ip-address.js';
+import * as gcp from "@pulumi/gcp";
+import { apiServices } from "../../google/api-services.js";
+import { provider } from "../../google/provider.js";
+import { ipAddress } from "./ip-address.js";
 
 const ingressIpAddress = ipAddress.address;
 
@@ -9,59 +9,63 @@ const ingressIpAddress = ipAddress.address;
 // who have their own DNS records poiting to bjerk.dev
 
 const legacyZone = new gcp.dns.ManagedZone(
-	'main-zone',
-	{
-		name: 'main-zone',
-		dnsName: 'flexisoft.bjerk.dev.',
-		description: 'Main zone',
-	},
-	{provider, dependsOn: apiServices},
+  "main-zone",
+  {
+    name: "main-zone",
+    dnsName: "flexisoft.bjerk.dev.",
+    description: "Main zone",
+  },
+  {
+    provider,
+    dependsOn: apiServices,
+    ignoreChanges: ["entity.managedZone.id"],
+  },
 );
 
 new gcp.dns.RecordSet(
-	'portal-app-ipv4',
-	{
-		managedZone: legacyZone.name,
-		name: 'flexisoft.bjerk.dev.',
-		type: 'A',
-		ttl: 300,
-		rrdatas: [ingressIpAddress],
-	},
-	{provider},
+  "portal-app-ipv4",
+  {
+    managedZone: legacyZone.name,
+    name: "flexisoft.bjerk.dev.",
+    type: "A",
+    ttl: 300,
+    rrdatas: [ingressIpAddress],
+  },
+  { provider },
 );
 
 new gcp.dns.RecordSet(
-	'debitor-portal-app-ipv4',
-	{
-		managedZone: legacyZone.name,
-		name: 'debitor.flexisoft.bjerk.dev.',
-		type: 'A',
-		ttl: 300,
-		rrdatas: [ingressIpAddress],
-	},
-	{provider},
+  "debitor-portal-app-ipv4",
+  {
+    managedZone: legacyZone.name,
+    name: "debitor.flexisoft.bjerk.dev.",
+    type: "A",
+    ttl: 300,
+    rrdatas: [ingressIpAddress],
+  },
+  { provider },
 );
 
 new gcp.dns.RecordSet(
-	'portal-api',
-	{
-		managedZone: legacyZone.name,
-		name: 'api.flexisoft.bjerk.dev.',
-		type: 'A',
-		ttl: 300,
-		rrdatas: [ingressIpAddress],
-	},
-	{provider},
+  "portal-api",
+  {
+    managedZone: legacyZone.name,
+    name: "api.flexisoft.bjerk.dev.",
+    type: "A",
+    ttl: 300,
+    rrdatas: [ingressIpAddress],
+  },
+  { provider },
 );
 
 new gcp.dns.RecordSet(
-	'studio',
-	{
-		managedZone: legacyZone.name,
-		name: 'studio.flexisoft.bjerk.dev.',
-		type: 'CNAME',
-		ttl: 300,
-		rrdatas: ['flexisoftorg.github.io.'],
-	},
-	{provider},
+  "studio",
+  {
+    managedZone: legacyZone.name,
+    name: "studio.flexisoft.bjerk.dev.",
+    type: "CNAME",
+    ttl: 300,
+    rrdatas: ["flexisoftorg.github.io."],
+  },
+  { provider },
 );
