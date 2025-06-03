ec8783e16d31bede58d25cc425f024f62ea2f7ae
Brage Sekse Aarset <brage.aarset@gmail.com>
2025-06-03 18:14:27 +0300

    feat: add more env variables to api (#745)
    * feat: add more env variables to api
    
    * update import
    
    * get cookie secret from the correct config

--- resources/kubernetes/api/api.ts
+++ resources/kubernetes/api/api.ts
@@ -1,4 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as kubernetes from "@pulumi/kubernetes";
 import { interpolate } from "@pulumi/pulumi";
 import { DeploymentComponent } from "../../components/deployment.js";
 import { apiDomain } from "../../config.js";
@@ -6,10 +7,28 @@ import { artifactRepoUrl } from "../../shared/google/artifact-registry.js";
 import { provider as kubernetesProvider } from "../../shared/kubernetes/provider.js";
 import { namespace } from "../namespace.js";
 import { registrationAppSanityCredentials } from "../registration-app/sanity-credentials.js";
+import { redis } from "../portal-api/redis.js";
 
 const config = new pulumi.Config("api");
+const portalApiConfig = new pulumi.Config("portal-api");
 
 const cleanApiDomain = apiDomain.slice(0, -1);
+
+const cookieSecret = portalApiConfig.requireSecret("cookie-secret");
+
+export const apiEnvSecrets = new kubernetes.core.v1.Secret(
+  "api-env-secrets",
+  {
+    metadata: {
+      name: "api-env-secrets",
+      namespace: namespace.metadata.name,
+    },
+    data: {
+      COOKIE_SECRET: cookieSecret,
+    },
+  },
+  { provider: kubernetesProvider },
+);
 
 export const Api = new DeploymentComponent(
   "api",
@@ -20,6 +39,16 @@ export const Api = new DeploymentComponent(
     namespace: namespace.metadata.name,
     envFrom: [
       { secretRef: { name: registrationAppSanityCredentials.metadata.name } },
+    ],
+    env: [
+      {
+        name: "REDIS_URL",
+        value: interpolate`${redis.service.metadata.name}.${redis.service.metadata.namespace}.svc.cluster.local:6379`,
+      },
+      {
+        name: "SELF_URL",
+        value: `https://${cleanApiDomain}`,
+      },
     ],
     port: 8000,
     resources: {
