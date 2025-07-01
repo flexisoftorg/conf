aeb9f9d2cc55ff7dde2832da7fb1615103356332
Brage Sekse Aarset <brage@bjerk.io>
2025-07-01 13:03:53 +0300

    feat: add allowed origins to api env

--- resources/kubernetes/api/api.ts
+++ resources/kubernetes/api/api.ts
@@ -8,6 +8,8 @@ import { provider as kubernetesProvider } from "../../shared/kubernetes/provider
 import { namespace } from "../namespace.js";
 import { registrationAppSanityCredentials } from "../registration-app/sanity-credentials.js";
 import { redis } from "../portal-api/redis.js";
+import { customers } from "../../get-customers.js";
+import { rootDomain } from "../../shared/config.js";
 
 const config = new pulumi.Config("api");
 
@@ -23,6 +25,20 @@ const cookieSecret = portalApiConfig.requireSecret("cookie-secret");
 const debitorPortalAppConfig = new pulumi.Config("debitor-portal-app");
 const user = debitorPortalAppConfig.requireSecret("database-user");
 const password = debitorPortalAppConfig.requireSecret("database-password");
+
+const cleanRootDomain = rootDomain.slice(0, -1);
+
+/** Comma-separated list of allowed origins, e.g. `"fpx.no, example.com, kvasir.no"` */
+const allowedOrigins = customers.apply((customers) => {
+  const customDomains = customers.reduce<string[]>((acc, customer) => {
+    const domain = customer.domain?.trim();
+    if (domain) {
+      acc.push(domain);
+    }
+    return acc;
+  }, []);
+  return [cleanRootDomain, ...customDomains].join(", ");
+});
 
 export const apiEnvSecrets = new kubernetes.core.v1.Secret(
   "api-env-secrets",
@@ -60,6 +76,10 @@ export const Api = new DeploymentComponent(
       {
         name: "SELF_URL",
         value: fullApiDomain,
+      },
+      {
+        name: "ALLOWED_ORIGINS",
+        value: allowedOrigins,
       },
     ],
     port: 8000,
