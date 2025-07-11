14e4b9255312058d7b8a3dcd739f279c1d668851
Brage Sekse Aarset <brage@bjerk.io>
2025-07-11 18:39:43 +0300

    fix: onboarding app domain

--- resources/kubernetes/onboarding/onboarding-app.ts
+++ resources/kubernetes/onboarding/onboarding-app.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import { interpolate } from "@pulumi/pulumi";
 import { DeploymentComponent } from "../../components/deployment.js";
-import { portalAppDomain } from "../../config.js";
+import { onboardingAppDomain } from "../../config.js";
 import { artifactRepoUrl } from "../../shared/google/artifact-registry.js";
 import { provider as kubernetesProvider } from "../../shared/kubernetes/provider.js";
 import { customerConfigMap } from "../customer-config.js";
@@ -10,14 +10,14 @@ import { onboardingAppDatabaseCredentials } from "./database-credentials.js";
 
 const config = new pulumi.Config("onboarding-app");
 
-const cleanPortalAppDomain = portalAppDomain.slice(0, -1);
+const cleanOnboardingAppDomain = onboardingAppDomain.slice(0, -1);
 
 export const onboardingApp = new DeploymentComponent(
   "onboarding-app",
   {
     image: interpolate`${artifactRepoUrl}/onboarding-app`,
     tag: config.require("tag"),
-    host: cleanPortalAppDomain,
+    host: cleanOnboardingAppDomain,
     namespace: namespace.metadata.name,
     port: 8000,
     envFrom: [
