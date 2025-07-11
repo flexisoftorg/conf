a4fbde4ce79875ae1fb762d4d9b5387e328eb24e
Brage Sekse Aarset <brage@bjerk.io>
2025-07-11 17:15:34 +0300

    feat: add onboarding app deployment

---
+++ resources/kubernetes/onboarding/database-credentials.ts
@@ -0,0 +1,24 @@
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import { provider } from "../../shared/kubernetes/provider.js";
+import { namespace } from "../namespace.js";
+
+const config = new pulumi.Config("onboarding-app");
+
+const user = config.requireSecret("database-user");
+const password = config.requireSecret("database-password");
+
+export const onboardingAppDatabaseCredentials = new k8s.core.v1.Secret(
+  "onboarding-app-database-credentials",
+  {
+    metadata: {
+      name: "registration-app-database-credentials",
+      namespace: namespace.metadata.name,
+    },
+    stringData: {
+      DATABASE_USER: user,
+      DATABASE_PASSWORD: password,
+    },
+  },
+  { provider },
+);
---
+++ resources/kubernetes/onboarding/onboarding-app.ts
@@ -0,0 +1,39 @@
+import * as pulumi from "@pulumi/pulumi";
+import { interpolate } from "@pulumi/pulumi";
+import { DeploymentComponent } from "../../components/deployment.js";
+import { portalAppDomain } from "../../config.js";
+import { artifactRepoUrl } from "../../shared/google/artifact-registry.js";
+import { provider as kubernetesProvider } from "../../shared/kubernetes/provider.js";
+import { customerConfigMap } from "../customer-config.js";
+import { namespace } from "../namespace.js";
+import { onboardingAppDatabaseCredentials } from "./database-credentials.js";
+
+const config = new pulumi.Config("onboarding-app");
+
+const cleanPortalAppDomain = portalAppDomain.slice(0, -1);
+
+export const onboardingApp = new DeploymentComponent(
+  "onboarding-app",
+  {
+    image: interpolate`${artifactRepoUrl}/onboarding-app`,
+    tag: config.require("tag"),
+    host: cleanPortalAppDomain,
+    namespace: namespace.metadata.name,
+    port: 8000,
+    envFrom: [
+      { configMapRef: { name: customerConfigMap.metadata.name } },
+      { secretRef: { name: onboardingAppDatabaseCredentials.metadata.name } },
+    ],
+    resources: {
+      requests: {
+        cpu: "250m",
+        memory: "512Mi",
+      },
+      limits: {
+        cpu: "250m",
+        memory: "512Mi",
+      },
+    },
+  },
+  { provider: kubernetesProvider },
+);
