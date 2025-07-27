a44ae763dfa4cbe00dbf6884999c44fa71d88b6f
Simen A. W. Olsen <hello@simenandre.no>
2025-07-27 18:57:11 +0200

    feat: add google storage bucket for debugging

---
+++ resources/google/debug-bucket.ts
@@ -0,0 +1,16 @@
+import * as gcp from '@pulumi/gcp';
+
+export const debugBucket = new gcp.storage.Bucket('debug-bucket', {
+  name: 'debug-bucket',
+  location: 'eu',
+  uniformBucketLevelAccess: true,
+  versioning: {
+    enabled: true,
+  },
+  lifecycleRules: [{
+    action: { type: 'Delete' },
+    condition: {
+      age: 30, // Delete objects older than 30 days
+    },
+  }],
+});
