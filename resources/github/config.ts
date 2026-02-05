e710c7674e79aee1047242f8102248500b4571b8
Brage Sekse Aarset <brage@bjerk.io>
2026-02-05 17:25:48 +0100

    feat: fetch github token from 1pass directly

--- resources/github/config.ts
+++ resources/github/config.ts
@@ -1,6 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
+import { getToken } from "../shared/op-secret.js";
 
 const config = new pulumi.Config("github");
+const opConfig = new pulumi.Config("op");
 
-export const token = config.requireSecret("flexisoftorg-token");
+export const token = getToken(
+	opConfig.require("flexisoftorg-github-token-path"),
+);
 export const owner = config.require("owner");
