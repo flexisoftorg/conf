b4635ee404d9db5170bc453155cfcea925d26532
Brage Sekse Aarset <brage@bjerk.io>
2026-04-24 16:14:20 +0300

    fix: remove legacy hosts

--- resources/kubernetes/portal-api/portal-api.ts
+++ resources/kubernetes/portal-api/portal-api.ts
@@ -37,7 +37,6 @@ export const portalApi = new DeploymentComponent(
 		image: interpolate`${artifactRepoUrl}/portal-api`,
 		tag: config.require("tag"),
 		host: cleanPortalApiDomain,
-		legacyHost: "api.flexisoft.bjerk.dev",
 		namespace: namespace.metadata.name,
 		port: 8000,
 		logLevel: config.get("log-level"),
